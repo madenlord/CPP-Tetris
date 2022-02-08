4f7d0ef300df815ba3adbc38553545c578fa7b70
Aaron Murillo <madenlordhobbies@gmail.com>
2022-02-08 20:54:27 +0100

    Added comments on main function to understand every step of the main program.

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,29 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200,200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Creates a window for 2D games
+    sf::RenderWindow window(sf::VideoMode(800,1000), "Tetris");
+    window.setFramerateLimit(60);
 
+    // Run the program as long as the window is open
     while(window.isOpen())
     {
+        // Check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while(window.pollEvent(event))
         {
+            // "Close requested" event: we close the window
             if(event.type == sf::Event::Closed)
                 window.close();
         }
 
+        // Clear the window with black color
         window.clear();
-        window.draw(shape);
+
+        // Draw everything here...
+        // window.draw(...);
+
+        // End the current frame
         window.display();
     }
 
